56ebbdf9e71c815901a29b1cc768bbaf2d9b22c6
Jose Campos <j.camposs2701@gmail.com>
2023-02-02 21:52:29 -0500

    Se realizan model de polls y se crear preguntas iniciales y su respuesta

--- premios/polls/models.py
+++ premios/polls/models.py
@@ -1,3 +1,28 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Question(models.Model):
+    
+    questions_text = models.CharField( max_length=200)
+    pub_date = models.DateTimeField("pub_date")
+    
+    def __str__(self):
+        return self.questions_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        
+    
+class Choice(models.Model):
+    
+    questions_text = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField("choices_text", max_length=200)
+    votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.choice_text
+    
