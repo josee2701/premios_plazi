1efff6c1eef4a6ddb3db511238555872b8c83384
Jose Campos <j.camposs2701@gmail.com>
2023-02-13 15:02:28 -0500

    Se realiza primera parte del proyecto

--- App/polls/views.py
+++ App/polls/views.py
@@ -1,21 +1,25 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.views import generic
 
 from .models import Choice, Question
 
 
-def index(request):
-    latest_question_list = Question.objects.all()
-    return render (request, "polls/index.html", {"latest_question_list":latest_question_list})
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+    
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]
 
+class DetailsView(generic.DetailView):
+    model = Question 
+    template_name = "polls/detail.html"
 
-def detail(request, question_id):
-    question= get_object_or_404(Question, pk =question_id)
-    return render(request, 'polls/detail.html', {'question':question})
-
-def results(request, question_id):
-    return HttpResponse("You're looking at the results of question %s." % question_id)
+class ResultView (generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
 
 def vote(request, question_id):
     question= get_object_or_404(Question, pk =question_id)
